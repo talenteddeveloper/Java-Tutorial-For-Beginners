83fa81a2d3c13530a21b58fb3bac1045e287041a
Talented Developer <alwaysrohitsingh@outlook.com>
2024-10-20 17:42:05 +0530

    if statement

---
+++ src/java_tutorial/IfStatement.java
@@ -0,0 +1,16 @@
+package java_tutorial;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		int number= 101;
+		if( number > 100) {
+			System.out.println("The number is +ve ");
+		}
+		
+		if( number % 2 == 0) {
+			System.out.println("The number is even");
+		}
+	}
+
+}
