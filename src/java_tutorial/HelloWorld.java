c52eec8d837e63533830a607f133785479096b56
Talented Developer <alwaysrohitsingh@outlook.com>
2024-10-21 20:46:53 +0530

    Java Data Types with Code Example

---
+++ src/java_tutorial/HelloWorld.java
@@ -0,0 +1,34 @@
+package java_tutorial;
+
+class HelloWorld {
+
+	public static void main(String[] args) {
+		byte age=127;
+		System.out.println(age);
+		
+		short range=9797;
+		System.out.println(range);
+		
+		int count=32234;
+		System.out.println(count);
+		
+		long ageLimit=12344L;
+		System.out.println(ageLimit);
+		
+		float countLimit=244.5f;
+		System.out.println(countLimit);
+		
+		double countData=4354435.4d;
+		System.out.println(countData);
+		
+		boolean isAvailable=true;
+		System.out.println(isAvailable);
+		
+		char printValue=65;
+		System.out.println(printValue);
+		
+		String name="";
+		
+	}
+
+}
