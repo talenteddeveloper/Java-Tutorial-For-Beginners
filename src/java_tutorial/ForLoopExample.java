2fa88791e7ce1a1ca3e6cda8028c9c0c7d6ee681
Talented Developer <alwaysrohitsingh@outlook.com>
2024-11-24 11:07:31 +0530

    How For loop works in Java

---
+++ src/java_tutorial/ForLoopExample.java
@@ -0,0 +1,17 @@
+package java_tutorial;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+//		for(int i=1; i<16; i++) {
+//			System.out.println(i);
+//		}
+		for(int i=16; i>0; i--) {
+			//System.out.println(i);
+			if(i==10) {
+				System.out.println("Hello");
+			}
+		}
+	}
+
+}
