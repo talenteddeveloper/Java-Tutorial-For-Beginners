90f6dcc96b2053f6531a920a55895a28fff2aec7
Talented Developer <alwaysrohitsingh@outlook.com>
2024-12-26 21:31:46 +0530

    While vs Do-While in Java: Key Differences Explained with Examples

---
+++ src/java_tutorial/WhileVsDoWhileExample.java
@@ -0,0 +1,21 @@
+package java_tutorial;
+
+public class WhileVsDoWhileExample {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		while(i<6) {
+			System.out.println(i);
+			i++;
+		}
+		
+		//do while
+		int j=13;
+		do {
+			System.out.println(j);
+			j++;	
+		}while(j<6);
+	}
+
+}
