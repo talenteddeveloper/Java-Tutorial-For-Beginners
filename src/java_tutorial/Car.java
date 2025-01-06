3b3706e0a7700a1d52401de7f9b31bfd00426d6d
Talented Developer <alwaysrohitsingh@outlook.com>
2025-01-06 10:02:15 +0530

    Creating Objects in Java Instance Variables and Methods Explained

---
+++ src/java_tutorial/Car.java
@@ -0,0 +1,13 @@
+package java_tutorial;
+
+public class Car {
+	// Instance variables
+	String brand;
+	String color;
+	int year;
+	
+	// Instance method
+	void startEngine() {
+		System.out.println(brand +" engine started");
+	}
+}
---
+++ src/java_tutorial/TestCar.java
@@ -0,0 +1,31 @@
+package java_tutorial;
+
+public class TestCar {
+
+	public static void main(String[] args) {
+		 // Creating the first object
+		Car car1 = new Car();
+		car1.brand="Tyota";
+		car1.color="Red";
+		car1.year=2020;
+		
+		// Creating the second object
+		Car car2 = new Car();
+		car2.brand="Honda";
+		car2.color="Blue";
+		car2.year=2022;
+		
+		 // Accessing the instance variables
+		System.out.println("Car 1: " + car1.brand + ", " + car1.color + ", " + car1.year);
+		
+		// Calling the instance method
+		car1.startEngine();
+		
+		// Accessing the instance variables
+		System.out.println("Car 2: " + car2.brand + ", " + car2.color + ", " + car2.year);
+		
+		// Calling the instance method
+		car2.startEngine();
+	}
+
+}
