facb963cad03e25e4d8dbf4aebc7e4e31e99b210
Talented Developer <alwaysrohitsingh@outlook.com>
2025-01-12 10:08:43 +0530

    Introduction to Arrays in Java

---
+++ src/java_tutorial/ArrayExample.java
@@ -0,0 +1,17 @@
+package java_tutorial;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[3];
+		numbers[0]=10;
+		numbers[1]=20;
+		numbers[2]=30;
+		
+		System.out.println(numbers[0]);//10
+		
+		int[] numbers1 = { 10,20,30};
+		System.out.println(numbers[2]); //30
+	}
+
+}
