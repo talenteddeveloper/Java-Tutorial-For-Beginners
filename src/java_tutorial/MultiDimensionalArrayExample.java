8075f9a062bfb359515a3b2e7c3b325ff1848c66
Talented Developer <alwaysrohitsingh@outlook.com>
2025-01-18 09:35:43 +0530

    Multi-Dimensional Arrays in Java

---
+++ src/java_tutorial/MultiDimensionalArrayExample.java
@@ -0,0 +1,22 @@
+package java_tutorial;
+
+public class MultiDimensionalArrayExample {
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+				{1,2,3,4},
+				{5,6,7,8},
+				{9,10,11,12}
+		};
+		//System.out.println(matrix[2][1]);
+		matrix[2][1] = 100;
+		for(int row=0; row< matrix.length; row++) {
+			for(int col=0; col<matrix[row].length;col++) {
+				//System.out.println(matrix[row][col] + "( "+row+" "+col+" )");
+				System.out.print(matrix[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
